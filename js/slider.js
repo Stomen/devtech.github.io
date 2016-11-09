8933cb13f07cc211edba6aaf844e31452d3095b6
stomen <ivan.krasovskij@gmail.com>
2016-11-09 23:07:46 +0200

    Initial commit

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.slider-main-photo').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        accessibility:false,
+        arrows: false,
+        speed: 1000,
+        dots: true,
+    });
+});
+$(document).ready(function(){
+    $('.works-slide').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        accessibility:false,
+        arrows: false,
+        speed: 1000,
+        dots: true,
+    });
+});
